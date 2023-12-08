aacc351d7885639be62f62f405efde9e4e259109
gruppa203 <gruppa203git@mail.ru>
2023-12-08 15:47:06 +0300

    Сессия 1

---
+++ Сессия 1/WpfKolpakov/WpfKolpakov/Pages/RoliPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfKolpakov.ApplicationData;
+
+namespace WpfKolpakov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RoliPage.xaml
+    /// </summary>
+    public partial class RoliPage : Page
+    {
+        public RoliPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToTovary_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.MainFrame.Navigate(new TovaryPage());
+        }
+    }
+}
---
+++ Сессия 1/WpfKolpakov/WpfKolpakov/Pages/TovaryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfKolpakov.ApplicationData;
+
+namespace WpfKolpakov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TovaryPage.xaml
+    /// </summary>
+    public partial class TovaryPage : Page
+    {
+        public TovaryPage()
+        {
+            InitializeComponent();
+            DGridTovary.ItemsSource = Bd_KolpakovEntities.GetContext().Tovary.ToList();
+        }
+
+        
+    }
+}
